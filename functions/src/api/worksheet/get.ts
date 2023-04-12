81f7f2440ccddefd87e7ed309b564ff95de08f1b
Daniel K. Guolo <daniel_kv@hotmail.com>
2023-04-12 10:16:05 -0300

    fix: firebase get days order by date

--- functions/src/api/worksheet/get.ts
+++ functions/src/api/worksheet/get.ts
@@ -5,23 +5,21 @@ import { init } from '../../helpers'
 
 init()
 
-export const getWorksheetById = https.onCall(
-    async (worksheetId: string, context: https.CallableContext) => {
-        const db = admin.firestore()
+export const getWorksheetById = https.onCall(async (worksheetId: string, context: https.CallableContext) => {
+    const db = admin.firestore()
 
-        const collection = db.collection('worksheets')
+    const collection = db.collection('worksheets')
 
-        const doc = await collection.doc(worksheetId).get()
+    const doc = await collection.doc(worksheetId).get()
 
-        if (!doc.exists) throw new Error('Worksheet not found')
+    if (!doc.exists) throw new Error('Worksheet not found')
 
-        return {
-            id: doc.id,
-            days: await getDays(doc.ref),
-            ...doc.data(),
-        }
+    return {
+        id: doc.id,
+        days: await getDays(doc.ref),
+        ...doc.data(),
     }
-)
+})
 
 export const getWorksheets = https.onCall(async (none: any, context: https.CallableContext) => {
     const db = admin.firestore()
@@ -35,7 +33,7 @@ export const getWorksheets = https.onCall(async (none: any, context: https.Calla
 })
 
 async function getDays(worksheetDocRef: admin.firestore.DocumentReference) {
-    const daysDocs = await worksheetDocRef.collection('days').get()
+    const daysDocs = await worksheetDocRef.collection('days').orderBy('date').get()
 
     return daysDocs.docs.map((doc) => ({
         id: doc.id,
