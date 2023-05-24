2d7d576f16c0cb80db39328690246176a56ce12e
Daniel K. Guolo <daniel_kv@hotmail.com>
2023-05-24 16:51:35 -0300

    init

--- functions/src/api/user/user.ts
+++ functions/src/api/user/user.ts
@@ -13,10 +13,42 @@ interface UserData {
 }
 
 export const createNewUser = https.onCall(async (data: UserData) => {
-    const auth = getAuth()
-
     try {
-        const newUser = await auth.createUser({ ...data, disabled: true })
+        const auth = getAuth()
+
+        const userData = {
+            ...data,
+            disabled: true,
+            subscription: {
+                name: 'free',
+                active: true,
+            },
+        }
+        const newUser = await auth.createUser(userData)
+
+        return {
+            uid: newUser.uid,
+            displayName: newUser.displayName,
+            email: newUser.email,
+        }
+    } catch (err) {
+        throw createHttpsError(err)
+    }
+})
+
+export const updateSubscription = https.onCall(async (data: UserData) => {
+    try {
+        const auth = getAuth()
+
+        const userData = {
+            ...data,
+            disabled: true,
+            subscription: {
+                name: 'free',
+                active: true,
+            },
+        }
+        const newUser = await auth.createUser(userData)
 
         return {
             uid: newUser.uid,
