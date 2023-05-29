dbc191cf15801ed0b182b589731e3dcbc17ebb9a
Daniel K. Guolo <daniel_kv@hotmail.com>
2023-05-29 16:12:56 -0300

    feat: user list

--- src/view/Login/index.tsx
+++ src/view/Login/index.tsx
@@ -4,19 +4,19 @@ import 'firebaseui/dist/firebaseui.css'
 import { Component, createEffect } from 'solid-js'
 
 import { firebaseProvider } from '@common/providers/firebase'
-import { setupFbAuthListener } from '@contexts/user/user.context'
 import { useNavigate } from '@solidjs/router'
 
 const LoginPage: Component = () => {
     const navigate = useNavigate()
-    setupFbAuthListener()
 
     createEffect(() => {
         const ui = authUI.AuthUI.getInstance() || new authUI.AuthUI(firebaseProvider.getAuth())
         ui.start('#loginForm', {
             signInOptions: firebaseProvider.signInOptions,
             callbacks: {
-                signInSuccessWithAuthResult: () => {
+                signInSuccessWithAuthResult: (auth) => {
+                    console.log(auth)
+
                     navigate('/')
                     return false
                 },
