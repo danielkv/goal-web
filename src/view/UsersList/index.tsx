f4b6542a46188fcf28b2aa84ac73f4ac7fc19069
Daniel K. Guolo <daniel_kv@hotmail.com>
2023-05-31 16:11:16 -0300

    fix: users list user initials

--- src/view/UsersList/index.tsx
+++ src/view/UsersList/index.tsx
@@ -48,7 +48,7 @@ function userInitials(name?: string): string {
 
     const splittedName = name.split(' ')
 
-    return `${splittedName[0][0]}${splittedName[1][0]}`
+    return `${splittedName[0]?.[0] || ''}${splittedName[1]?.[0] || ''}`
 }
 
 const UsersList: Component = () => {
